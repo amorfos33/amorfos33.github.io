4265dde66fe43a07d98af1e5de19809444f24ebd
amorfos33 <amorfos33@gmail.com>
2018-03-15 17:43:33 +0200

    15.03.2018

---
+++ project_Ethanol/js/feature.js
@@ -0,0 +1,10 @@
+function openbox(id) {
+  var all = document.querySelectorAll(".slidePartTextBlok");
+  for (var i = 0; i < all.length; i++) {
+    if (all[i].id == id) {
+      all[i].style.display = (all[i].style.display == 'none')? 'block' : 'none';
+    } else {
+      all[i].style.display = 'none';
+    }
+  }
+}
